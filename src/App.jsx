3b8bdbca586eb7ae2c511799d8704a89526fbdbc
Ajarek <ajarek@poczta.onet.pl>
2023-02-18 23:01:33 +0100

    added navigation & router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import {Routes,Route } from "react-router-dom";
+import Navigation from './components/Navigation/Navigation'
 import Facture from './pages/Facture/Facture'
+import Home from './pages/Home/Home'
 function App() {
   return <div className='App'>
-    <Facture/>
+    <Navigation/>
+    <Routes>
+    <Route
+          path='/'
+          element={<Home />}
+        />
+    <Route
+          path='/facture'
+          element={<Facture/>}
+        />
+    
+    </Routes>
   </div>
 }
 
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import './Navigation.css'
+import { NavLink } from 'react-router-dom'
+import Hamburger from 'hamburger-react'
+const Navigation = () => {
+  const [isOpen, setOpen] = useState(false)
+  return (
+    <nav className={'nav'}>
+      <a
+        className={'link'}
+        href='mailto:ajarek@poczta.onet.pl'
+      >
+        <div className='logo'>
+          <img
+            src='logo.png'
+            alt='logo'
+          />
+          @jarek
+        </div>
+      </a>
+      <ul className={!isOpen ? 'wrapper' : 'wrapper navbar-none'}>
+        <NavLink
+          className={'link'}
+          to='/'
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={'link'}
+          to='/facture'
+        >
+          Faktura
+        </NavLink>
+      
+      </ul>
+      <div className='hamburger'>
+        <Hamburger
+          size={30}
+          duration={0.3}
+          distance='md'
+          color={isOpen ? '#f15e50' : '#080808'}
+          easing='ease-in'
+          rounded
+          label='Show menu'
+          onToggle={(toggled) => {
+            setOpen(true)
+            if (toggled) {
+              // open a menu
+            } else {
+              setOpen(false)
+            }
+          }}
+        />
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation
